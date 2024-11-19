f221ab710be108dfc989c2d523a50a570095ba73
Rakun <jnicolaybv@gmail.com>
2024-11-18 22:06:03 -0500

    Add ContactoComponent and configure routing
    - Created ContactoComponent for the contact page.
    - Updated app.routes.ts to include routing for ContactoComponent.
    - Modified main.ts to provide router configuration.
    - Updated app.component.ts to use RouterModule.
    - Enhanced index.html with Bootstrap and updated title.
    - Added global styles in styles.css for better UI.
    - Created pelicula2.html for movie details display.
    - Added unit tests for ContactoComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ProyectoAngular';
+  title = 'Pelicritica';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'contacto', component: ContactoComponent },
+    
+];
